b94e9c547316460ebf4351a22a56c61a1ecbfed9
Anton <kiselevtony@gmail.com>
2021-07-16 19:12:02 -0400

    Initial Commit

---
+++ ToxicQuiz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToxicQuiz
+//
+//  Created by ANTON KISELEV on 7/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToxicQuiz/ToxicQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToxicQuizApp.swift
+//  ToxicQuiz
+//
+//  Created by ANTON KISELEV on 7/16/21.
+//
+
+import SwiftUI
+
+@main
+struct ToxicQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
