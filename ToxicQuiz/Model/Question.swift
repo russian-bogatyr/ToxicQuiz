43439fd45feb8bca09a3bfceffe841bcf44dcc38
Anton <kiselevtony@gmail.com>
2021-07-16 19:51:01 -0400

    Initial creation

---
+++ ToxicQuiz/Model/Question.swift
@@ -0,0 +1,41 @@
+//
+//  Question.swift
+//  ToxicQuiz
+//
+//  Created by ANTON KISELEV on 7/16/21.
+//
+
+import Foundation
+
+struct QuizBrain {
+    let quiz = [
+        Question(q: "I tend take responsibility for the feelings and / or behavior of others"),
+        Question(q: "I find it difficult to identify my feelings, such as happiness, anger, embarrassment, discouragement, or excitement"),
+        Question(q: "I find it hard to express my feelings"),
+        Question(q: "I feel fear or anxiety at the thought of how others will react to my feelings or behavior"),
+        Question(q: ""),
+        Question(q: ""),
+        Question(q: ""),
+        Question(q: ""),
+        Question(q: ""),
+        Question(q: ""),
+        Question(q: ""),
+        Question(q: ""),
+        Question(q: ""),
+        Question(q: ""),
+        Question(q: ""),
+        Question(q: ""),
+        Question(q: ""),
+        Question(q: ""),
+        Question(q: ""),
+        Question(q: ""),
+        Question(q: ""),
+        Question(q: ""),
+        Question(q: ""),
+        Question(q: ""),
+        Question(q: ""),
+        Question(q: ""),
+        
+    
+    ]
+}
---
+++ ToxicQuiz/Model/QuizBrain.swift
@@ -0,0 +1,16 @@
+//
+//  QuizBrain.swift
+//  ToxicQuiz
+//
+//  Created by ANTON KISELEV on 7/16/21.
+//
+
+import Foundation
+
+struct Question{
+    let text : String
+    
+    init(q: String){
+        text = q
+    }
+}
